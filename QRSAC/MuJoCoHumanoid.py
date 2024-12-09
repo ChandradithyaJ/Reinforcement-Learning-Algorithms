50d76f612afa206daebbe5a33af305e8f7c34a87
Chandradithya J <110807345+ChandradithyaJ@users.noreply.github.com>
2024-12-09 11:54:23 +0530

    Logging rewards

--- QRSAC/MuJoCoHumanoid.py
+++ QRSAC/MuJoCoHumanoid.py
@@ -2,7 +2,7 @@ import gymnasium as gym
 import nnabla_rl.algorithms.qrsac as qrsac
 from nnabla_rl.environments.wrappers.gymnasium import Gymnasium2GymWrapper
 import nnabla_rl.hooks as hooks
-from SaveRewardsHook import EpisodeRewardLogger
+from SaveRewardsWriter import FileWriter
 
 def build_env(env_name, seed=None):
     env = gym.make(env_name, render_mode='human')
@@ -15,26 +15,26 @@ def main():
     env = build_env(env_name, seed=1002)
 
     results_folder = "./QRSAC/results/"
-    reward_file = results_folder + env_name
+    rewards_writer = FileWriter(outdir=results_folder, file_prefix=env_name)
 
     # initialize hooks for logging
     iteration_num_hook = hooks.IterationNumHook(timing=100)
-    episode_reward_hook = EpisodeRewardLogger(reward_file, env)
+    iteration_state_hook = hooks.IterationStateHook(writer=rewards_writer, timing=100)
 
     config = qrsac.QRSACConfig(
         batch_size=128,
         environment_steps=1,
-        gradient_steps=1,
+        gradient_steps=3,
         initial_temperature=0.1,
-        fix_temperature=True,
+        fix_temperature=False,
         start_timesteps=200,
         replay_buffer_size=int(1e6),
-        num_steps=3,
+        num_steps=5,
         num_quantiles=16,
         kappa=1.0
     )
     QRSAC_algo = qrsac.QRSAC(env, config=config)
-    QRSAC_algo.set_hooks(hooks=[iteration_num_hook, episode_reward_hook])
+    QRSAC_algo.set_hooks(hooks=[iteration_num_hook, iteration_state_hook])
 
     QRSAC_algo.train(env, total_iterations=int(1e6))
 
