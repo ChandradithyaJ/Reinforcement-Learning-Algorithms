c389dcc7413d771707433657cde14f6fa0b285a9
Chandradithya J <110807345+ChandradithyaJ@users.noreply.github.com>
2024-12-09 12:19:09 +0530

    Update AtariKungFuMaster.py

--- QRSAC/AtariKungFuMaster.py
+++ QRSAC/AtariKungFuMaster.py
@@ -2,34 +2,39 @@ import gymnasium as gym
 import nnabla_rl.algorithms.qrsac as qrsac
 from nnabla_rl.environments.wrappers.gymnasium import Gymnasium2GymWrapper
 import nnabla_rl.hooks as hooks
+from SaveRewardsWriter import FileWriter
 
-def build_env(seed=None):
-    env = gym.make('ALE/KungFuMaster-v5', render_mode='human')
+def build_env(env_name, seed=None):
+    env = gym.make(env_name, render_mode='human')
     env = Gymnasium2GymWrapper(env)
     env.seed(seed)
     return env
 
 def main():
-    env = build_env(seed=1002)
+    env_name = "ALE/KungFuMaster-v5"
+    env = build_env(env_name, seed=1002)
+
+    results_folder = "./QRSAC/results/"
+    rewards_writer = FileWriter(outdir=results_folder, file_prefix="ALE-KungFuMaster-v5")
 
     # initialize hooks for logging
     iteration_num_hook = hooks.IterationNumHook(timing=100)
-    save_snapshot_hook = hooks.SaveSnapshotHook("./QRSAC/results/ALE-KungFuMasterv5-Results", timing=1000)
+    iteration_state_hook = hooks.EvaluationHook(env, writer=rewards_writer, timing=100)
 
     config = qrsac.QRSACConfig(
         batch_size=128,
         environment_steps=1,
-        gradient_steps=1,
+        gradient_steps=3,
         initial_temperature=0.1,
-        fix_temperature=True,
+        fix_temperature=False,
         start_timesteps=200,
         replay_buffer_size=int(1e6),
-        num_steps=3,
+        num_steps=5,
         num_quantiles=16,
         kappa=1.0
     )
     QRSAC_algo = qrsac.QRSAC(env, config=config)
-    QRSAC_algo.set_hooks(hooks=[iteration_num_hook, save_snapshot_hook])
+    QRSAC_algo.set_hooks(hooks=[iteration_num_hook, iteration_state_hook])
 
     QRSAC_algo.train(env, total_iterations=int(1e6))
 
